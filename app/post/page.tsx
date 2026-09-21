3a46977a58546dc8edbbacb3e66d82dcefefe350
Haier <haier@gmail.com>
2026-09-21 18:53:10 +0500

    fix import error

--- app/post/page.tsx
+++ app/post/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState } from "react";
-import { supabase } from "@/lib/supabaseclient";
+import { supabase } from "../lib/supabaseclient";
 
 export default function PostPage(){
 const [form,setForm]=useState({
